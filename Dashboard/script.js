49f8e42cd7d19f39206a85352ebb8a6e1003c8b0
aabhi <aabhi@gmail.com>
2026-06-11 02:45:04 +0530

    Initial project setup and dashboard UI

---
+++ Dashboard/script.js
@@ -0,0 +1,1 @@
+console.log("Smart Home Energy Saver Dashboard Loaded");
---
+++ ESP32_Code/energy_saver.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("Smart Home Energy Saver System Running");
+  delay(2000);
+}
